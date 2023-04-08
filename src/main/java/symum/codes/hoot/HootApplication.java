68c89c50244947cd24b78fdc52382317028eab27
Symum Ahmed <symumahmed21@gmail.com>
2023-04-08 20:23:58 +0600

    Added backend codes

---
+++ src/main/java/symum/codes/hoot/HootApplication.java
@@ -0,0 +1,14 @@
+package symum.codes.hoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class HootApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/symum/codes/hoot/HootApplicationTests.java
@@ -0,0 +1,13 @@
+package symum.codes.hoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
